1d62323d03e0f8d19d0d216a82fe2abf2a511a55
collectorstar <duy57665@nuce.edu.vn>
2023-08-27 11:28:44 +0700

    tao migra va model, controler

---
+++ database/migrations/2023_08_27_034507_create_dealers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dealers', function (Blueprint $table) {
+            $table->id();
+            $table->text('dealerName');
+            $table->boolean('gender');
+            $table->string('phoneNumber');
+            $table->dateTime('dateOfBirth');
+            $table->text('country');
+            $table->text('specificAddress');
+            $table->text('businessItem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dealers');
+    }
+};
