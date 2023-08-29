0675cbe976260a90a60a8b65c8dff8bdb65e3795
d1agnoze <dacvuong202@gmail.com>
2023-08-29 17:10:15 +0700

    complete login screen

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {   
+        return view('auth.login',['title'=>'Trang đăng nhập']);
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ])) {
+            //echo "đăng nhập thành công";
+            session()->flash('success', 'Logged in');
+            return redirect()->route('admin');
+        }
+        Session()->flash('error', 'Tên đăng nhập hoặc mật khẩu không chính xác');
+        return redirect()->back();
+    }
+}
