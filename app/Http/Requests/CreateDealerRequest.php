6ae8ad62f3ec739231a2511cc325d321a0d40bab
d1agnoze <dacvuong202@gmail.com>
2023-08-31 17:37:07 +0700

    add vài cái linh tinh

--- app/Http/Requests/CreateDealerRequest.php
+++ app/Http/Requests/CreateDealerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CreateDealerRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class CreateDealerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,25 @@ function DeleteDealer(id, url){
         })
     }
 };
+/**
+ * @author vdac
+ * @param {*} type HTTP request type
+ * @param {*} data payload (khởi tạo thành object trước khi truyền)
+ * @param {*} url  url
+ * @param {*} callbackOnSuccess function cần thực hiện khi request thành công, mặc định sẽ thông báo qua toastr
+ * @param {*} datatype kiểu dữ liệu, mặc định JSON
+ * @param {*} actionString string thao tác, sẽ gắp string này với template thông báo trên toastr
+ * @example sendAjax('DELETE', {id: 1}, "google.com", undefined, undefined, "Thao tác");
+ */
+const sendAjax = (type, data, url, callbackOnSuccess, datatype = JSON, actionString = "Thao tác") => {
+    //skip opt params ---> undefined
+    $.ajax({
+        type: type,
+        datatype:datatype,
+        data:data,
+        url:url,
+        success: callbackOnSuccess ? callbackOnSuccess : toastr.success(`${actionString} thành công`),
+        error: toastr.error(`${actionString} không thành công`)
+    })
+}
+
