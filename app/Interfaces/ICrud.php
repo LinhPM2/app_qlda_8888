5cafaa68b651541f73bd73afbcd311f97ef26106
d1agnoze <dacvuong202@gmail.com>
2023-09-05 18:49:55 +0700

    update on Icrud

--- app/Interfaces/ICrud.php
+++ app/Interfaces/ICrud.php
@@ -7,4 +7,6 @@ interface ICrud
     public function create_resource($resource);
     public function edit_resource($resource);
     public function delete_resource($resourceId);
+    public function get_resource($resourceId);
+    public function get_all_resources();
 }
